41ca89ffa375e3bc9cfed2b475425f3b578dd596
Michael <kllmmc23@gmail.com>
2020-11-24 07:34:49 -0700

    ListScreenUpdate

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -3,34 +3,39 @@ import { View, Text, StyleSheet, FlatList } from "react-native";
 
 const ListScreen = () => {
   const friends = [
-    { name: "Friend #1" },
-    { name: "Friend #2" },
-    { name: "Friend #3" },
-    { name: "Friend #4" },
-    { name: "Friend #5" },
-    { name: "Friend #6" },
-    { name: "Friend #7" },
-    { name: "Friend #8" },
-    { name: "Friend #9" },
+    { name: "Friend #1", age: 27 },
+    { name: "Friend #2", age: 21 },
+    { name: "Friend #3", age: 32 },
+    { name: "Friend #4", age: 44 },
+    { name: "Friend #5", age: 28 },
+    { name: "Friend #6", age: 19 },
+    { name: "Friend #7", age: 64 },
+    { name: "Friend #8", age: 33 },
+    { name: "Friend #9", age: 52 },
   ];
   return (
     <FlatList
-      horizontal
+      //horizontal
       showsHorizontalScrollIndicator={false}
       keyExtractor={({ name }) => name}
       data={friends}
       renderItem={({ item }) => {
-        return <Text style={styles.textStyle}>{item.name}</Text>;
+        return (
+          <View>
+            <Text>List Screen</Text>
+            <Text style={styles.textStyle}>
+              {item.name} - Age {item.age}
+            </Text>
+          </View>
+        );
       }}
-    >
-      <Text>List Screen</Text>
-    </FlatList>
+    ></FlatList>
   );
 };
 
 const styles = StyleSheet.create({
   textStyle: {
-    marginVertical: 50,
+    marginVertical: 5,
   },
 });
 
